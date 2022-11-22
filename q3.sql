627a77328be676698ce02aa20b7c2a46adf442ec
Yan xu <90088728+Yanfors@users.noreply.github.com>
2022-11-22 15:52:05 +0800

    Delete q3.sql

--- q3.sql
+++
@@ -1,6 +0,0 @@
-select CompanyName||"|"||printf("%.2f",count(case when ShippedDate > RequiredDate then 1 else null end)/round(count('Order'.Id))*100)  
-AS result_q3 
-from "Order" left join Shipper        
-where "Order".shipVia = Shipper.Id  
-group by Shipper.Id  
-order by (count(case when ShippedDate > RequiredDate then 1 else null end)/round(count('Order'.Id))*100) desc;
