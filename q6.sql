3f789d29120008271696e27fcd5e2068d2cddc2c
Yan xu <90088728+Yanfors@users.noreply.github.com>
2022-11-22 15:52:31 +0800

    Delete q6.sql

--- q6.sql
+++
@@ -1,7 +0,0 @@
-select Id||"|"||OrderDate||"|"||(lag(OrderDate,1,orderDate) over(order by OrderDate))||"|"||  
-round(julianday(OrderDate)-julianday(lag(OrderDate,1,orderDate) over(ORDER BY OrderDate)),2)  
-as result_q6     
-from 'Order'                                                                                  
-where CustomerId="BLONP"                                                                      
-order by OrderDate                                                                            
-limit 10;
