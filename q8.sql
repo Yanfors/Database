cc666a9247988b4cf8dfd568e7b6517d87a4e1bf
Yan xu <90088728+Yanfors@users.noreply.github.com>
2022-11-22 15:52:48 +0800

    Delete q8.sql

--- q8.sql
+++
@@ -1,9 +0,0 @@
-select RegionDescription||"|"||FirstName||"|"||LastName||"|"||BirthDate 
-as result_q8
-from(select * from Employee 
-              left join EmployeeTerritory on Employee.Id=EmployeeTerritory.employeeId       
-              left join Territory on EmployeeTerritory.TerritoryId=Territory.Id       
-              left join Region on Territory.RegionId=Region.Id       
-    order by Employee.BirthDate desc) 
-group by RegionId  
-order by RegionId; 
