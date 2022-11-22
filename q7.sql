1cf04f3fa2d26a53ae0be6043dd7ae0810bf0e34
Yan xu <90088728+Yanfors@users.noreply.github.com>
2022-11-22 15:52:39 +0800

    Delete q7.sql

--- q7.sql
+++
@@ -1,8 +0,0 @@
-select CompanyName||"|"||CustomerId||"|"||round(exp,2)
-from
-(select ifnull(CompanyName, "MISSING_NAME") as CompanyName, CustomerId, exp, NTILE(4) over(order by CAST(exp AS float)) as quartile 
-     FROM
-     (select "Order".CustomerId, sum((UnitPrice*Quantity)) as exp from "Order" left join OrderDetail on "Order".Id = OrderDetail.OrderId group by "Order".CustomerId)
-     left join Customer on CustomerId = Customer.Id) 
- where quartile=1
- order by CAST(exp AS float);
